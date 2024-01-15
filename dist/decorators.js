7a9921c36f06cb4f8b5f6681501396782b89d3e9
Colson <colsoncalifornication@gmail.com>
2024-01-15 16:09:01 +0545

    decorator factory + advanced decorator dom template element replacer

--- dist/decorators.js
+++ dist/decorators.js
@@ -1,21 +1,52 @@
 "use strict";
+/*
+function Logger(constructor: Function) {
+  console.log('Logger ran -> Logging...');
+  console.log(constructor);
+}
+*/
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log('Logger ran -> Logging...');
-    console.log(constructor);
+/* @ Decorator Factory */
+function Logger(logString) {
+    console.log('LOGGER FACTORY');
+    return function (constructor) {
+        console.log('Rendering logger');
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+// Advanced decorator func
+function WithTemplate(template, hookId) {
+    console.log('TEMPLATE FACTORY');
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(..._) {
+                super();
+                console.log('Rendering template...');
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1').textContent = this.name;
+                }
+            }
+        };
+    };
 }
 let Person = class Person {
     constructor() {
-        this.name = 'COLSON';
+        this.name = 'ROLLS ROYCE';
         console.log('calling constructor...');
         console.log('Creating person object....');
     }
 };
 Person = __decorate([
-    Logger
+    WithTemplate('<h1>My person object</h1>', 'root'),
+    Logger('nigga logged decorator @LOGGER')
 ], Person);
+const person = new Person();
+console.log('lmaooo', person);
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,14 +1,54 @@
+/*
 function Logger(constructor: Function) {
   console.log('Logger ran -> Logging...');
   console.log(constructor);
 }
+*/
 
-@Logger
+/* @ Decorator Factory */
+function Logger(logString: string) {
+  console.log('LOGGER FACTORY');
+
+  return function (constructor: Function) {
+    console.log('Rendering logger');
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+// Advanced decorator func
+function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
+
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T,
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+
+        console.log('Rendering template...');
+        const hookEl = document.getElementById(hookId);
+
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
+
+@WithTemplate('<h1>My person object</h1>', 'root')
+@Logger('nigga logged decorator @LOGGER')
 class Person {
-  name = 'COLSON';
+  name = 'ROLLS ROYCE';
 
   constructor() {
     console.log('calling constructor...');
     console.log('Creating person object....');
   }
 }
+
+const person = new Person();
+console.log('lmaooo', person);
